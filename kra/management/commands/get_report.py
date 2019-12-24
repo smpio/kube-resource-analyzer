94ecbc48d9d1bbfec75c69ec1905fd337cdc8ab0
Dmitry Bashkatov <dbashkatov@gmail.com>
2019-12-24 22:16:07 +0300

    get_report: group by container and add requests/limits

--- kra/management/commands/get_report.py
+++ kra/management/commands/get_report.py
@@ -13,17 +13,37 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for wl in models.Workload.objects.order_by('namespace', 'name'):
-            resource_usage_qs = models.ResourceUsage.objects.filter(container__pod__workload=wl)
-
-            stat = Struct(resource_usage_qs.aggregate(max_memory_mi=Max('memory_mi'), avg_cpu_m=Avg('cpu_m')))
-            if stat.avg_cpu_m is not None:
-                stat.avg_cpu_m = int(stat.avg_cpu_m)
-
             print(f'{wl.kind.name} {wl.namespace}/{wl.name}')
-            if stat.max_memory_mi is not None:
-                print(f'\tmax memory: {stat.max_memory_mi} Mi')
-            if stat.avg_cpu_m is not None:
-                print(f'\tavg CPU: {stat.avg_cpu_m}m')
+
+            resource_usage_qs = models.ResourceUsage.objects\
+                .filter(container__pod__workload=wl)\
+                .values('container__name')\
+                .annotate(max_memory_mi=Max('memory_mi'), avg_cpu_m=Avg('cpu_m'))
+
+            for container_resource_usage in resource_usage_qs:
+                stat = Struct(container_resource_usage)
+
+                last_container = models.Container.objects\
+                    .filter(pod__workload=wl, name=stat.container__name)\
+                    .order_by('pod__started_at')\
+                    .last()
+
+                print(f'  {stat.container__name}')
+
+                if stat.avg_cpu_m is not None:
+                    stat.avg_cpu_m = int(stat.avg_cpu_m)
+
+                if stat.max_memory_mi is not None:
+                    msg = f'    max memory: {stat.max_memory_mi} Mi'
+                    if last_container and last_container.memory_limit_mi:
+                        msg += f' / {last_container.memory_limit_mi} Mi'
+                    print(msg)
+
+                if stat.avg_cpu_m is not None:
+                    msg = f'    avg CPU: {stat.avg_cpu_m}m'
+                    if last_container and last_container.cpu_request_m:
+                        msg += f' / {last_container.cpu_request_m}m'
+                    print(msg)
 
 
 class Struct:
