9961ddf16522d7955fc2692eda9858be7e14bd3d
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-05-25 17:28:28 +0300

    optimize sql

--- kra/tasks/make_summary.py
+++ kra/tasks/make_summary.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 from django.db import transaction
 from django.db.models import Max, Avg
 
+from utils.django.db import bulk_save
+
 from kra import models
 
 log = logging.getLogger(__name__)
@@ -20,26 +22,27 @@ def make_summary():
     for u in resource_usage_qs:
         resource_usage[u['container__pod__workload_id']].append(u)
 
-    for wl in models.Workload.objects.order_by('namespace', 'name'):
-        try:
-            workload_ru = resource_usage[wl.id]
-        except KeyError:
-            continue
+    with bulk_save() as save:
+        for wl in models.Workload.objects.order_by('namespace', 'name'):
+            try:
+                workload_ru = resource_usage[wl.id]
+            except KeyError:
+                continue
 
-        for cu in workload_ru:
-            stat = models.Summary(workload=wl, container_name=cu['container__name'])
-            stat.max_memory_mi = cu['max_memory_mi']
-            if cu['avg_cpu_m'] is not None:
-                stat.avg_cpu_m = int(cu['avg_cpu_m'])
+            for cu in workload_ru:
+                stat = models.Summary(workload=wl, container_name=cu['container__name'])
+                stat.max_memory_mi = cu['max_memory_mi']
+                if cu['avg_cpu_m'] is not None:
+                    stat.avg_cpu_m = int(cu['avg_cpu_m'])
 
-            last_container = models.Container.objects\
-                .filter(pod__workload=wl, name=stat.container_name)\
-                .order_by('-pod__started_at')\
-                .first()
+                last_container = models.Container.objects\
+                    .filter(pod__workload=wl, name=stat.container_name)\
+                    .order_by('-pod__started_at')\
+                    .first()
 
-            if last_container is not None:
-                stat.memory_limit_mi = last_container.memory_limit_mi
-                stat.cpu_request_m = last_container.cpu_request_m
+                if last_container is not None:
+                    stat.memory_limit_mi = last_container.memory_limit_mi
+                    stat.cpu_request_m = last_container.cpu_request_m
 
-            stat.save()
-            yield stat
+                save(stat)
+                yield stat
