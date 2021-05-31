23019cbdba67c1eaa491e68bfe801b72712f754d
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-05-31 19:56:52 +0300

    more accurate suggestion priorities

--- kra/tasks/make_suggestions.py
+++ kra/tasks/make_suggestions.py
@@ -25,8 +25,19 @@ def make_suggestions():
     for e in oom_qs:
         oom_events[(e.workload_id, e.container.name)].append(e)
 
+    max_memory_mi = 0
+    max_cpu_m = 0
+    summaries = list(models.Summary.objects.all())
+    for stat in summaries:
+        max_memory_mi = max(max_memory_mi, stat.max_memory_mi)
+        if stat.memory_limit_mi:
+            max_memory_mi = max(max_memory_mi, stat.memory_limit_mi)
+        max_cpu_m = max(max_cpu_m, stat.avg_cpu_m)
+        if stat.cpu_request_m:
+            max_cpu_m = max(max_cpu_m, stat.cpu_request_m)
+
     with bulk_save() as save:
-        for stat in models.Summary.objects.all():
+        for stat in summaries:
             try:
                 sug = suggestions[stat.id]
             except KeyError:
@@ -47,14 +58,14 @@ def make_suggestions():
                     min_memory_limit = int(oom.container.memory_limit_mi * memory_reserve_multiplier) + 1
                     if stat.memory_limit_mi < min_memory_limit:
                         new_memory_limits_mi.append(min_memory_limit)
-                        priorities.append(200 + pp(min_memory_limit / stat.memory_limit_mi))
+                        priorities.append(200 + pp(min_memory_limit / max_memory_mi))
                         memory_reasons.append(f'OOM @ {oom.container.memory_limit_mi} Mi limit')
 
             min_memory_limit = int(stat.max_memory_mi * memory_reserve_multiplier) + 1
             if stat.memory_limit_mi:
                 if stat.memory_limit_mi < min_memory_limit:
                     new_memory_limits_mi.append(min_memory_limit)
-                    priorities.append(pp(min_memory_limit / stat.memory_limit_mi))
+                    priorities.append(pp(min_memory_limit / max_memory_mi))
                     memory_reasons.append(f'memory usage {stat.max_memory_mi} Mi near limit {stat.memory_limit_mi} Mi')
             else:
                 new_memory_limits_mi.append(min_memory_limit)
@@ -64,7 +75,7 @@ def make_suggestions():
                 max_cpu_usage = stat.cpu_request_m * cpu_overuse_multiplier
                 if stat.avg_cpu_m > max_cpu_usage:
                     new_cpu_requests_m.append(stat.avg_cpu_m)
-                    priorities.append(pp(stat.avg_cpu_m / stat.cpu_request_m))
+                    priorities.append(pp(stat.avg_cpu_m / max_cpu_m))
                     cpu_reasons.append(f'avg cpu usage {stat.avg_cpu_m}m exceeds request {stat.cpu_request_m}m too much')
             else:
                 new_cpu_requests_m.append(stat.avg_cpu_m)
@@ -93,6 +104,6 @@ def make_suggestions():
 
 
 def percent_priority(ratio):
-    if ratio > 2:
-        ratio = 2
-    return (ratio - 1) * 100
+    if ratio > 1:
+        ratio = 1
+    return ratio * 100
