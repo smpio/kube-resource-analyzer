c15ff5c9db70777ec9658fbc193a6859add4241c
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-06-09 20:35:57 +0300

    remove debug log

--- kra/collectors/pods.py
+++ kra/collectors/pods.py
@@ -155,8 +155,7 @@ def update_containers(pod, mypod):
     for name, data in mycontainers.items():
         runtime_id = data.pop('runtime_id', None)
         if not runtime_id:
-            log.debug('%s', pod.to_dict())
-            log.warning('No runtime_id for container %s in pod %s/%s', name, pod.metadata.namespace, pod.metadata.name)
+            log.info('No runtime_id for container %s in pod %s/%s', name, pod.metadata.namespace, pod.metadata.name)
             continue
         if not data.get('started_at'):
             log.warning('No started_at for container %s in pod %s/%s', name, pod.metadata.namespace, pod.metadata.name)
