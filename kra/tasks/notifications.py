c076c55521464041ef0f45f69c552db391cdc606
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-06-12 02:52:03 +0300

    tasks.notification: ignore if DISCORD_WEBHOOK_URL not set

--- kra/tasks/notifications.py
+++ kra/tasks/notifications.py
@@ -6,6 +6,8 @@ from kra.celery import task
 
 @task
 def post(message, username='KRA'):
+    if not settings.DISCORD_WEBHOOK_URL:
+        return
     resp = requests.post(settings.DISCORD_WEBHOOK_URL, json={
         'username': username,
         'content': message,
