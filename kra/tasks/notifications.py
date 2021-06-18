53011d2545619cdc343904bc21cab62fedea04da
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-06-18 15:22:10 +0300

    remove Discord webhook usage
    see #46

--- kra/tasks/notifications.py
+++ kra/tasks/notifications.py
@@ -1,21 +1,8 @@
 import datetime
 
-import requests
-from django.conf import settings
 import kubernetes.client as api
 
 from kra.celery import task
-
-
-@task
-def post(message, username='KRA'):
-    if not settings.DISCORD_WEBHOOK_URL:
-        return
-    resp = requests.post(settings.DISCORD_WEBHOOK_URL, json={
-        'username': username,
-        'content': message,
-    })
-    resp.raise_for_status()
 
 
 @task
