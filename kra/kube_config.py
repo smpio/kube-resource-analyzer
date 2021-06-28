8119e27855e42f37d586678be555d0f7c3a90bcb
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-06-28 12:36:41 +0300

    use utils.kubernetes.config

--- kra/kube_config.py
+++ kra/kube_config.py
@@ -1,13 +1,7 @@
-import kubernetes
 from django.conf import settings
+
+from utils.kubernetes.config import configure
 
 
 def init():
-    if settings.KUBE_API_URL:
-        configuration = kubernetes.client.Configuration()
-        configuration.host = settings.KUBE_API_URL
-        kubernetes.client.Configuration.set_default(configuration)
-    elif settings.KUBE_IN_CLUSTER:
-        kubernetes.config.load_incluster_config()
-    else:
-        kubernetes.config.load_kube_config()
+    configure(settings.KUBE_API_URL, settings.KUBE_IN_CLUSTER)
