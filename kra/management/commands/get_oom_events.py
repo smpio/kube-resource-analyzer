39cd49de60dfde51e4113eea22f6d66b09532032
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-07-12 17:30:32 +0300

    improve commands output

--- kra/management/commands/get_oom_events.py
+++ kra/management/commands/get_oom_events.py
@@ -13,9 +13,13 @@ class Command(BaseCommand):
         for ev in qs.iterator():
             wl = ev.container.pod.workload
             if ev.victim_comm:
-                msg = f'killed {ev.victim_comm}({ev.victim_pid or "?"})'
+                msg = f'killed {ev.victim_comm}'
+                if ev.victim_pid:
+                    msg += f'({ev.victim_pid})'
             elif ev.target_comm:
-                msg = f'kill {ev.target_comm}({ev.target_pid or "?"})'
+                msg = f'kill {ev.target_comm}'
+                if ev.target_pid:
+                    msg += f'({ev.target_pid})'
             else:
                 msg = ''
             print(f'{ev.happened_at} {wl.kind.name} {wl.namespace}/{wl.name}: {msg}')
